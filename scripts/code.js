341e208c977ffba7f5364b891b415a5ba0ae7c9c
Kayla <kaylai.johnson@yahoo.com>
2026-02-10 13:09:20 -0500

    Creating logic for the computer choice

--- scripts/code.js
+++ scripts/code.js
@@ -1,1 +1,11 @@
 console.log("Hello World");
+
+function getComputerChoice(){
+    x = (Math.floor(Math.random() * 100) % 3) + 1;
+    if(x == 1) return "Rock";
+    if(x == 2) return "Paper";
+    if(x == 3) return "Scissors";
+    else return 0;
+}
+
+console.log(getComputerChoice());
