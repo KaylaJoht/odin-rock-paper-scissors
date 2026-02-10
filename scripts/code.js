8acf476151a30b8d7131a4aea060c8ef7a720877
Kayla <kaylai.johnson@yahoo.com>
2026-02-10 13:46:25 -0500

    finished basic one-round version

--- scripts/code.js
+++ scripts/code.js
@@ -5,9 +5,9 @@ let computerScore = 0;
 
 function getComputerChoice(){
     x = (Math.floor(Math.random() * 100) % 3) + 1;
-    if(x == 1) return "Rock";
-    if(x == 2) return "Paper";
-    if(x == 3) return "Scissors";
+    if(x == 1) return "rock";
+    if(x == 2) return "paper";
+    if(x == 3) return "scissors";
     else return 0;
 }
 
@@ -17,7 +17,31 @@ function getHumanChoice(){
     return y;
 }
 
-console.log("Human score ", humanScore);
+function playRound(human, computer){
+    human = human.toLowerCase();
+    console.log(human);
+    console.log(computer);
+    result = false;
+
+    if((human == "rock" && computer == "paper")
+        || (human == "paper" && computer == "scissors")
+        || (human == "scissors" && computer == "rock")){
+            computerScore += 1;
+            return `You lose! ${computer} beats ${human}`;
+        }
+    else if (human == computer) return "You tied!";
+    else {
+        humanScore += 1;
+        return `You win! ${human} beats ${computer}`;
+    }
+
+}
+
+console.log("Human Score ", humanScore);
 console.log("Computer Score ", computerScore);
-console.log(getComputerChoice());
-console.log(getHumanChoice());
+//console.log("The computer chose ", getComputerChoice());
+//console.log("You chose ", getHumanChoice());
+//console.log(playRound(getHumanChoice(), getComputerChoice()));
+console.log(playRound("paper", "rock"));
+console.log(playRound("scissors", "scissors"));
+console.log(playRound("rock", "paper"));
