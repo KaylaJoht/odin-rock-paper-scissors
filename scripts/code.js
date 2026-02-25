5cb3b13356f9c49942a708ac59daa0ec66262972
Kayla <kaylai.johnson@yahoo.com>
2026-02-25 13:04:59 -0500

    Fixed the gameplay loop to be based around buttons

--- scripts/code.js
+++ scripts/code.js
@@ -10,36 +10,36 @@ const sBtn = document.getElementById('s');
 pBtn.addEventListener('click', (event) => {
         val = pBtn.textContent;
         console.log(val);
+        playGame(val);
 });
 
 rBtn.addEventListener('click', (event) => {
         val = rBtn.textContent;
         console.log(val);
+        playGame(val);
 });
 
 sBtn.addEventListener('click', (event) => {
         val = sBtn.textContent;
         console.log(val);
+        playGame(val);
 });
-
-
 
 
 function getComputerChoice(){
     x = (Math.floor(Math.random() * 100) % 3) + 1;
-    if(x == 1) return "rock";
-    if(x == 2) return "paper";
-    if(x == 3) return "scissors";
+    if(x == 1) return "Rock";
+    if(x == 2) return "Paper";
+    if(x == 3) return "Scissors";
     else return 0;
 }
 
 function playRound(human, computer){
-    human = human.toLowerCase();
     result = false;
 
-    if((human == "rock" && computer == "paper")
-        || (human == "paper" && computer == "scissors")
-        || (human == "scissors" && computer == "rock")){
+    if((human == "Rock" && computer == "Paper")
+        || (human == "Paper" && computer == "Scissors")
+        || (human == "Scissors" && computer == "Rock")){
             computerScore += 1;
             return `You lose! ${computer} beats ${human}`;
         }
@@ -57,22 +57,19 @@ function getWinner(){
     else return `You tied ${humanScore}:${computerScore}`
 }
 
-function playGame(){
+function playGame(human){
     console.log(`Round ${i + 1}`);
     console.log("Human Score ", humanScore);
     console.log("Computer Score ", computerScore);
-    human = getHumanChoice();
     computer = getComputerChoice();
     console.log(`You chose ${human}`);
     console.log(`The computer chose ${computer}`);
 
     console.log(playRound(human, computer));
-    //i += 1;
+    i += 1;
 
     //if(i < 5) playGame();
     return "Game Over! That was fun!"
 }
 
 console.log("Let's play Rock Paper Scissors");
-console.log(playGame());
-console.log(getWinner());
